b57128a054767e1c75e379f4544701d9c347dced
sephirothson <sephirothson@163.com>
2014-11-09 13:43:07 +0800

    Initial Commit

---
+++ TaskListApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskListApp
+//
+//  Created by  sephiroth on 14/11/9.
+//  Copyright (c) 2014年 sephiroth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
